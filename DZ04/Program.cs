62883ae6c9af96164b96c7cd23fc4af8913d81de
Алексей <ezekiil@yandex.ru>
2023-04-15 04:02:34 +0300

    добавлено ДЗ

---
+++ DZ04/Program.cs
@@ -0,0 +1,18 @@
+﻿/* напишите программу, которая на вход принимает три числа
+и выдает максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine() ?? "");
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine() ?? "");
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine() ?? "");
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("максимальное число " + max);
---
+++ DZ13/Program.cs
@@ -0,0 +1,19 @@
+﻿/* напишите программу, которая выводит третью цифру заданного числа
+или сообщает, что третьей цифры нет */
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int numDigit = Convert.ToString(N).Length; // количество цифр в числе
+if (numDigit <= 2) // проверка на трехзначное число
+{
+Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    if (numDigit > 3) // проверка на трехзначное число
+        {
+        N = N / Convert.ToInt32(Math.Pow(10, numDigit - 3)); // вычисление третьей цифры введённого числа
+        }
+    N = N % 10;
+    Console.WriteLine($"Третья цифра введённого числа: {N}");
+}
