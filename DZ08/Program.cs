6ec5aa7e21ca7d0a849a6354d3db8ee4c25b1bba
Алексей <ezekiil@yandex.ru>
2023-04-17 02:31:19 +0300

    added comments

--- DZ08/Program.cs
+++ DZ08/Program.cs
@@ -8,9 +8,6 @@ int N = Convert.ToInt32(Console.ReadLine());
 int i = 2;
 while (i <= N)
 {
-    if (i % 2 == 0)
-    {
-        Console.Write(i + " ");
-    }
-    i++;
+    Console.Write(i + " ");
+    i+=2;
 }
