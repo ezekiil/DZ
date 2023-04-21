40f5ce99608a3a9f09b80ce8aae87974e50a4dbb
Алексей <ezekiil@yandex.ru>
2023-04-21 19:47:55 +0300

    add DZ 19,21,23

---
+++ DZ19/Program.cs
@@ -0,0 +1,20 @@
+﻿/* напишите программу, которая принимает на вход пятизначное число
+и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+void Palindrome(int number)
+{
+    if (number / 10000 == number % 10 && number / 1000 % 10 == number / 10 % 10)
+    {
+        Console.WriteLine($"{number} - палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - не палиндром");
+    }
+}
+
+Console.WriteLine("Введите пятизначное число: ");
+Palindrome(int.Parse(Console.ReadLine()!));
---
+++ DZ23/Program.cs
@@ -0,0 +1,16 @@
+﻿/* напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1,8,27,64,125 */
+
+void Cube(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        Console.WriteLine(Math.Pow(i, 3));
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Cube(number);
